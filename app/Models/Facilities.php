6540559604fe8ff3e85fa4e099cd48ede1027ac8
ernestnash <eernestnnashon@gmail.com>
2023-04-13 13:25:40 +0300

    new migrations

---
+++ app/Models/Facilities.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Facilities extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'name',
+        'abbreviation',
+        'description',
+        'location_desc',
+        'number_of_beds',
+        'number_of_cots',
+        'open_whole_day',
+        'open_whole_week',
+        'facility_type',
+        'operation_status',
+        'ward',
+        'owner',
+        'officer_in_charge',
+        'physical_address',
+        'parent'
+    ];
+}
